d2f64dc98b848b759c1799f07270695ad9737dd6
Lewis <40667682@live.napier.ac.uk>
2024-02-07 13:41:30 +0630

    Added comment in "CityMethod".

--- src/main/java/com/napier/grp1/CityMethod.java
+++ src/main/java/com/napier/grp1/CityMethod.java
@@ -7,44 +7,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class CityMethod {
-    public ArrayList<City> getCities(Connection con) {
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT city.Name, city.CountryCode, country.Continent, city.District, city.Population, country.Name "
-                            + "FROM city, country "
-                            + "WHERE city.CountryCode = country.Code "
-                            + "ORDER BY city.Population DESC ";
-
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            // Extract city information
-            ArrayList<City> cities = new ArrayList<City>();
-            while (rset.next()) {
-                City cit = new City();
-                cit.setCity_name(rset.getString("city.Name"));
-                cit.setCountry_code(rset.getString("city.CountryCode"));
-                cit.setCountry_name(rset.getString("country.Name"));
-                cit.setCity_continent(rset.getString("country.Continent"));
-                cit.setCity_district(rset.getString("city.District"));
-                cit.setCity_population(rset.getInt("city.Population"));
-                cities.add(cit);
-            }
-            return cities;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get city details");
-            return null;
-        }
-    }
-
+    //Method to catch and sort all cities data with the inputted country from the database
     public ArrayList<City> getCitiesByCountry(Connection con, String cityCountry) {
         try {
-
 
             // Create string for SQL statement
             String strSelect =
